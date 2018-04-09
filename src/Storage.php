4c2922a1fca25bcec5a8ff32674fb4ab2f0c4e86
刘广财 <itxiao6@qq.com>
2018-04-09 17:42:19 +0800

    初始提交

--- src/Storage.php
+++ src/Storage.php
@@ -5,15 +5,24 @@ namespace Itxiao6\Storage;
  * Interface Storage
  * @package Itxiao6\Storage
  */
+
+use Itxiao6\Storage\Disk\Qiniu;
+
 /**
  * Class Storage
  * @package Itxiao6\Storage
  */
 abstract class Storage
 {
+    /**
+     * @param $name
+     * @param $arguments
+     * @return \Itxiao6\Storage\Disk\Interfaces
+     */
     public function __call($name, $arguments)
     {
 //        TODO 装饰者模式
+//        return new self();
     }
     public static function __callStatic($name, $arguments)
     {
